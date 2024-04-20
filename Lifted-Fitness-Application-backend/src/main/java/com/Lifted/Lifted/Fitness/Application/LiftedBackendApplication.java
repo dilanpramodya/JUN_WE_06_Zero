d3b9572bf9ba3b0ee56e00569b434f479f4008be
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2024-04-20 14:24:06 +0530

    first commit

---
+++ Lifted-Fitness-Application-backend/src/main/java/com/Lifted/Lifted/Fitness/Application/LiftedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Lifted.Lifted.Fitness.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftedBackendApplication.class, args);
+	}
+
+}
